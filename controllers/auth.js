25ffdab770a8a5803f929ef1c378791dcbe4adcd
Phonekham <phone.madridista@gmail.com>
2021-09-20 15:41:57 +0700

    Global state, CSRF, axios interceptors and protected routes

--- controllers/auth.js
+++ controllers/auth.js
@@ -62,3 +62,21 @@ export const login = async (req, res) => {
     return res.status(400).send("Error. Try again.");
   }
 };
+
+export const logout = async (req, res) => {
+  try {
+    res.clearCookie("token");
+    return res.json({ message: "Signout success" });
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+export const currentUser = async (req, res) => {
+  try {
+    const user = await User.findById(req.user._id).select("-password").exec();
+    return res.json(user);
+  } catch (err) {
+    console.log(err);
+  }
+};
