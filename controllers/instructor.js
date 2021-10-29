6efda4ce489f594e3c7aeaa7dea03fa7f104dd52
Phonekham <phone.madridista@gmail.com>
2021-10-29 18:17:21 +0700

    .

--- controllers/instructor.js
+++ controllers/instructor.js
@@ -15,7 +15,7 @@ export const makeInstructor = async (req, res) => {
       user.save();
     }
     // 3. create account link based on account id (for frontend to complete onboarding)
-    const accountLink = await stripe.accountLinks.create({
+    let accountLink = await stripe.accountLinks.create({
       account: user.stripe_account_id,
       refresh_url: process.env.STRIPE_REDIRECT_URL,
       return_url: process.env.STRIPE_REDIRECT_URL,
